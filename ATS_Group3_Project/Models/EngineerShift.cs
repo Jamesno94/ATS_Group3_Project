dc3e90a15def434a613060c5b61fd49cf248fdef
Jamesno94 <jamesno94@icloud.com>
2026-04-28 14:46:01 +0100

    update 1

---
+++ ATS_Group3_Project/Models/EngineerShift.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ATS_Group3_Project;
+using System.Data.Entity;
+
+public class EngineerShift
+{
+    [Key]
+    public int ShiftId { get; set; }
+
+    public string StaffId { get; set; }
+
+    public string ShiftType { get; set; }
+
+    public bool Monday { get; set; }
+    public bool Tuesday { get; set; }
+    public bool Wednesday { get; set; }
+    public bool Thursday { get; set; }
+    public bool Friday { get; set; }
+    public bool Saturday { get; set; }
+    public bool Sunday { get; set; }
+
+    public virtual Staff Staff { get; set; }
+}
---
+++ ATS_Group3_Project/Models/JobRecord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ATS_Group3_Project;
+using System.Data.Entity;
+
+
+public class JobRecord
+{
+    [Key]
+    public int JobId { get; set; }
+
+    public DateTime JobDate { get; set; }
+
+    public string JobTime { get; set; }
+
+    public string JobType { get; set; }
+
+    public string FaultDescription { get; set; }
+
+    public bool GeneratorServiced { get; set; }
+    public bool GearboxServiced { get; set; }
+
+    public bool GeneratorReplaced { get; set; }
+    public bool GearboxReplaced { get; set; }
+
+    public string JobComplete { get; set; }
+
+    public string StaffId { get; set; }
+    public string TurbineId { get; set; }
+
+    public virtual Staff Staff { get; set; }
+
+    public virtual Turbine Turbine { get; set; }
+}
