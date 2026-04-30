032349c8bc963eae0b6d34eb6310beefbc59f6d3
305174031 <30517403@cityofglacol.ac.uk>
2026-04-30 14:43:47 +0100

    Testing GitHub

---
+++ ATS_Group3_Project/Models/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS_Group3_Project.Models
+{
+    public class Test
+    {
+        public string Testing { get; set; }
+
+        Testing = "THis is a test of github";
+    }
+}
