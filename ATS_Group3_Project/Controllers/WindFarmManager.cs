d369d34e0be3b3eca3f4cd3c3ea8c0ef4730ead8
Jamesno94 <jamesno94@icloud.com>
2026-05-01 13:06:34 +0100

    Managers updated, new jobRecord class as old one was missing attributes.
    ALL MANAGER CODE REQUIRES REVIEW

--- ATS_Group3_Project/Controllers/WindFarmManager.cs
+++ ATS_Group3_Project/Controllers/WindFarmManager.cs
@@ -1,12 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ATS_Group3_Project.Controllers
+namespace ATS_Group3_Project
 {
-    internal class WindFarmManager
+    public class WindFarmManager
     {
+        public List<WindFarm> GetAllWindFarms()
+        {
+            using (var db = new ATSContext())
+            {
+                return db.WindFarms
+                    .Include(w => w.Turbines)
+                    .ToList();
+            }
+        }
+
+        public WindFarm GetWindFarmById(int farmId)
+        {
+            using (var db = new ATSContext())
+            {
+                return db.WindFarms
+                    .Include(w => w.Turbines)
+                    .FirstOrDefault(w => w.FarmId == farmId);
+            }
+        }
+
+        public bool AddWindFarm(WindFarm farm)
+        {
+            using (var db = new ATSContext())
+            {
+                db.WindFarms.Add(farm);
+                db.SaveChanges();
+                return true;
+            }
+        }
+
+        public bool UpdateWindFarm(WindFarm updatedFarm)
+        {
+            using (var db = new ATSContext())
+            {
+                var farm = db.WindFarms.Find(updatedFarm.FarmId);
+
+                if (farm == null)
+                {
+                    return false;
+                }
+
+                farm.FarmName = updatedFarm.FarmName;
+                farm.Address1 = updatedFarm.Address1;
+                farm.Address2 = updatedFarm.Address2;
+                farm.City = updatedFarm.City;
+                farm.Postcode = updatedFarm.Postcode;
+                farm.Region = updatedFarm.Region;
+
+                db.SaveChanges();
+                return true;
+            }
+        }
+
+        public bool DeleteWindFarm(int farmId)
+        {
+            using (var db = new ATSContext())
+            {
+                var farm = db.WindFarms.Find(farmId);
+
+                if (farm == null)
+                {
+                    return false;
+                }
+
+                db.WindFarms.Remove(farm);
+                db.SaveChanges();
+                return true;
+            }
+        }
     }
 }
