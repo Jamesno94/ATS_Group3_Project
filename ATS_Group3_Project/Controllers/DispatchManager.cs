de0197c299f6e1781af5eb729e2bdc44c089f43b
Jamesno94 <jamesno94@icloud.com>
2026-05-01 13:32:40 +0100

    added a dispatch manager

---
+++ ATS_Group3_Project/Controllers/DispatchManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS_Group3_Project.Controllers
+{
+    internal class DispatchManager
+    {
+    }
+}
--- ATS_Group3_Project/Models/Staff.cs
+++ ATS_Group3_Project/Models/Staff.cs
@@ -18,6 +18,8 @@ public class Staff
     [Required]
     public string WorkMobile { get; set; }
 
+    public string HomeMobile { get; set; }
+
     [Required]
     public string Email { get; set; }
 
