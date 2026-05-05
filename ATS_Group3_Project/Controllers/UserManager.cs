8ba7052fc06e8f64f6e9ea1a54df000c2e786268
Jamesno94 <jamesno94@icloud.com>
2026-05-05 12:56:38 +0100

    Fixed Cascade issue

--- ATS_Group3_Project/Controllers/UserManager.cs
+++ ATS_Group3_Project/Controllers/UserManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text.RegularExpressions;
+using ATS_Group3_Project;
 
 public class UserManager
 {
--- ATS_Group3_Project/Data/ATSContext.cs
+++ ATS_Group3_Project/Data/ATSContext.cs
@@ -1,17 +1,56 @@
 ﻿using System.Data.Entity;
-using ATS_Group3_Project;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
-public class ATSContext : DbContext
+namespace ATS_Group3_Project
 {
-    public ATSContext() : base("ATSConnection")
+    public class ATSContext : DbContext
     {
-    }
+        public ATSContext() : base("ATSConnection")
+        {
+        }
 
-    public DbSet<WindFarm> WindFarms { get; set; }
-    public DbSet<Turbine> Turbines { get; set; }
-    public DbSet<Staff> Staff { get; set; }
-    public DbSet<EngineerShift> EngineerShifts { get; set; }
-    public DbSet<JobRecord> JobRecords { get; set; }
-    public DbSet<User> Users { get; set; }
+        public DbSet<WindFarm> WindFarms { get; set; }
+        public DbSet<Turbine> Turbines { get; set; }
+        public DbSet<Staff> Staff { get; set; }
+        public DbSet<EngineerShift> EngineerShifts { get; set; }
+        public DbSet<JobRecord> JobRecords { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            modelBuilder.Entity<Turbine>()
+                .HasRequired(t => t.WindFarm)
+                .WithMany(w => w.Turbines)
+                .HasForeignKey(t => t.FarmId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<JobRecord>()
+                .HasRequired(j => j.WindFarm)
+                .WithMany()
+                .HasForeignKey(j => j.FarmId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<JobRecord>()
+                .HasRequired(j => j.Turbine)
+                .WithMany()
+                .HasForeignKey(j => j.TurbineId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<JobRecord>()
+                .HasRequired(j => j.Staff)
+                .WithMany()
+                .HasForeignKey(j => j.StaffId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<User>()
+                .HasRequired(u => u.Staff)
+                .WithMany()
+                .HasForeignKey(u => u.StaffId)
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
 }
