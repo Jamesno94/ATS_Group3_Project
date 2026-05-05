dd23a95438bd2063a629f233c6cd7671ad69261d
Jamesno94 <jamesno94@icloud.com>
2026-05-05 13:21:11 +0100

    StaffManager

--- ATS_Group3_Project/Controllers/StaffManager.cs
+++ ATS_Group3_Project/Controllers/StaffManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 
@@ -6,13 +7,91 @@ namespace ATS_Group3_Project
 {
     public class StaffManager
     {
+        public bool RegisterStaffAccount(string staffId, string firstName, string lastName, string workMobile, string homeMobile, string email, string address1,
+    string address2, string city, string postcode, decimal salary, string role, string password)
+        {
+            using (var db = new ATSContext())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        if (StaffExists(staffId) || EmailExists(email))
+                            return false;
+
+                        Staff staff = new Staff
+                        {
+                            StaffId = staffId.Trim(),
+                            FirstName = firstName.Trim(),
+                            LastName = lastName.Trim(),
+                            WorkMobile = workMobile.Trim(),
+                            HomeMobile = homeMobile,
+                            Email = email.Trim(),
+                            Address1 = address1.Trim(),
+                            Address2 = address2,
+                            City = city.Trim(),
+                            Postcode = postcode.Trim(),
+                            Salary = salary,
+                            Role = role
+                        };
+
+                        var passwordResult = PasswordHelper.HashPassword(password);
+
+                        User user = new User
+                        {
+                            StaffId = staff.StaffId,
+                            PasswordHash = passwordResult.hash,
+                            PasswordSalt = passwordResult.salt
+                        };
+
+                        db.Staff.Add(staff);
+                        db.Users.Add(user);
+                        db.SaveChanges();
+
+                        transaction.Commit();
+                        return true;
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        return false;
+                    }
+                }
+            }
+        }
+
+        public bool AddStaff(Staff staff)
+        {
+            using (var db = new ATSContext())
+            {
+                if (db.Staff.Any(s => s.StaffId == staff.StaffId))
+                    return false;
+
+                db.Staff.Add(staff);
+                db.SaveChanges();
+                return true;
+            }
+        }
+
         public List<Staff> GetAllStaff()
         {
             using (var db = new ATSContext())
             {
                 return db.Staff
                     .Include(s => s.EngineerShift)
+                    .Include(s => s.Jobs)
                     .ToList();
+            }
+        }
+
+        public Staff GetStaffById(string staffId)
+        {
+            using (var db = new ATSContext())
+            {
+                return db.Staff
+                    .Include(s => s.EngineerShift)
+                    .Include(s => s.Jobs)
+                    .FirstOrDefault(s => s.StaffId == staffId);
             }
         }
 
@@ -37,23 +116,13 @@ namespace ATS_Group3_Project
             }
         }
 
-        public Staff GetStaffById(string staffId)
+        public List<Staff> GetAllAdmins()
         {
             using (var db = new ATSContext())
             {
                 return db.Staff
-                    .Include(s => s.EngineerShift)
-                    .FirstOrDefault(s => s.StaffId == staffId);
-            }
-        }
-
-        public bool AddStaff(Staff staff)
-        {
-            using (var db = new ATSContext())
-            {
-                db.Staff.Add(staff);
-                db.SaveChanges();
-                return true;
+                    .Where(s => s.Role == "Admin")
+                    .ToList();
             }
         }
 
@@ -64,9 +133,7 @@ namespace ATS_Group3_Project
                 var staff = db.Staff.Find(updatedStaff.StaffId);
 
                 if (staff == null)
-                {
                     return false;
-                }
 
                 staff.FirstName = updatedStaff.FirstName;
                 staff.LastName = updatedStaff.LastName;
@@ -92,13 +159,33 @@ namespace ATS_Group3_Project
                 var staff = db.Staff.Find(staffId);
 
                 if (staff == null)
-                {
                     return false;
-                }
+
+                var user = db.Users.FirstOrDefault(u => u.StaffId == staffId);
+
+                if (user != null)
+                    db.Users.Remove(user);
 
                 db.Staff.Remove(staff);
                 db.SaveChanges();
+
                 return true;
+            }
+        }
+
+        public bool StaffExists(string staffId)
+        {
+            using (var db = new ATSContext())
+            {
+                return db.Staff.Any(s => s.StaffId == staffId);
+            }
+        }
+
+        public bool EmailExists(string email)
+        {
+            using (var db = new ATSContext())
+            {
+                return db.Staff.Any(s => s.Email == email);
             }
         }
     }
